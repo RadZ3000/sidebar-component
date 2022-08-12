0959b9e870b6c36aa28575d9685ab3c9b101b83e
RadZ3000 <herald.adowei@gmail.com>
2022-08-12 08:20:57 -0500

    Edited index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
     <div>
 
       <div className="min-h-screen w-20 bg-gray-700 text-slate-300 flex flex-col items-center">
-        <a href="#" className="flex justify-center w-full bg-red-500 py-3 text-4xl text-black hover:text-white"><FaComments /></a>
+        <a href="#" className="flex justify-center w-full bg-orange-400 py-3 text-4xl text-black hover:text-white"><FaComments /></a>
         <a href="#" className="btn"><FaBriefcase /></a>
         <a href="#" className="btn"><FaAddressBook /></a>
         <a href="#" className="btn"><FaCalendar /></a>
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
         <a href="#" className="btn"><FaStar /></a>
         <a href="#" className="btn"><FaSlidersH /></a>
         <a href="#" className="btn"><FaHeadset /></a>
-        <div className="flex justify-center"><a href="#" className="absolute bottom-10 rounded-full bg-white text-black w-max h-max text-xl p-3"><FaUser /></a></div>
+        <div className="flex justify-center"><a href="#" className="absolute bottom-10 rounded-full bg-white text-black w-max h-max text-xl p-3 hover:bg-black hover:text-white"><FaUser /></a></div>
       </div>
 
     </div>
